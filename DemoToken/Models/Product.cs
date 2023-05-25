4a8bc67d67649406106eeeb7e2eb60287c19c553
TristanNgo <ngotrungtin123456789@gmail.com>
2023-05-25 23:41:34 +0700

    edit product

--- DemoToken/Models/Product.cs
+++ DemoToken/Models/Product.cs
@@ -6,6 +6,7 @@
         public string Name { get; set; }
         public decimal Price { get; set; }
         public int Stock { get; set; }
-        public decimal cash { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
     }
 }
