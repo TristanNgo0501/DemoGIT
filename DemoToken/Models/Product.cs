26f5650e52e31e3ddf3814d1ca60fae716f82efb
Huỳnh Nhật Hiếu <76414823+Iloveminimalism@users.noreply.github.com>
2023-05-25 23:50:53 +0700

    add ImageUrl

--- DemoToken/Models/Product.cs
+++ DemoToken/Models/Product.cs
@@ -8,5 +8,6 @@
         public int Stock { get; set; }
         public string Description { get; set; }
         public string Image { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
