9a717685c869e95d3a48845812b0ff8555ade309
TristanNgo <ngotrungtin123456789@gmail.com>
2023-05-25 23:48:48 +0700

    add prop Product

--- DemoToken/Models/Product.cs
+++ DemoToken/Models/Product.cs
@@ -8,5 +8,7 @@
         public int Stock { get; set; }
         public string Description { get; set; }
         public string Image { get; set; }
+        public string ImageUrl { get; set; }
+        public string ImageType { get; set; }
     }
 }
