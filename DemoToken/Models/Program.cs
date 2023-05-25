b99509c9e850481ecd8dfd95095cf6024f4939ec
QuangNam014 <tuquangnam1911@gmail.com>
2023-05-26 00:34:14 +0700

    create model program

---
+++ DemoToken/Models/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace DemoToken.Models
+{
+    public class Program
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Desciption { get; set; }
+    }
+}
